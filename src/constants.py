f1ffab876440d26e9ea5da8f9996fa7b51e535b1
iisharankov <ivansharankov3@gmail.com>
2021-11-15 12:21:12 +0100

    can now handle relative imports to load absolute file locations

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,14 @@
-TRAIN_DATASET = "data/train.csv"
-TEST_DATASET = "data/test.csv"
+import os
+
+# Find absolute path to this file so we can import data from anywhere in the repo
+abs_path = os.path.dirname(os.path.abspath(__file__))
+
+TRAIN_DATASET = abs_path + "/data/train.csv"
+TEST_DATASET = abs_path + "/data/test.csv"
+
+
+# CNN
+CNN_LR = 0.5
+CNN_EPOCHS = 1000
+CNN_DISPLAY_EPOCHS = 50
+CNN_V = 2 # Verbosity
