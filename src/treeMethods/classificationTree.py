224634770e67a23e54ddce7ce37d28ebeea22e35
iisharankov <ivansharankov3@gmail.com>
2021-12-06 10:22:41 +0100

    final cleanup of classification trees

--- src/treeMethods/classificationTree.py
+++ src/treeMethods/classificationTree.py
@@ -4,10 +4,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
-
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 
 import src.constants as const
@@ -21,7 +21,7 @@ def clean_x_and_y(data):
 
 
 def classification_tree():
-    clf = sklearn.tree.DecisionTreeClassifier(max_depth=7, random_state=0)
+    clf = sklearn.tree.DecisionTreeClassifier(max_depth=7, random_state=R)
     clf.fit(X_train, Y_train)
     # importances = pd.DataFrame({'feature': X_train.columns, 'importance': np.round(clf.feature_importances_, 3)})
     # importances = importances.sort_values('importance', ascending=False)
@@ -31,61 +31,79 @@ def classification_tree():
 
 
 def random_forest():
-    rfc = sklearn.ensemble.RandomForestClassifier(n_estimators=50, criterion='entropy', random_state=4)
+    rfc = RandomForestClassifier(n_estimators=n, criterion='entropy', random_state=R)
     rfc.fit(X_train, Y_train)
     return rfc
 
+
 def bagging():
-    bg_pipeline = make_pipeline(StandardScaler(), LogisticRegression(random_state=1))
+    bg_pipeline = make_pipeline(StandardScaler(), LogisticRegression(random_state=R))
+    bgclassifier = BaggingClassifier(base_estimator=bg_pipeline, n_estimators=n,
+                                     max_features=X_train.shape[1], max_samples=100,
+                                     random_state=R, n_jobs=5)
 
-    bgclassifier = sklearn.ensemble.BaggingClassifier(
-        base_estimator=bg_pipeline, n_estimators=100,
-        max_features=X_train.shape[1], max_samples=100,
-        random_state=12, n_jobs=5
-    )
-
-    # Fit the bagging classifier
     bgclassifier.fit(X_train, Y_train)
     return bgclassifier
 
 
-def cross_validation(model, x, y, n):
-    cv = sklearn.model_selection.KFold(n_splits=n, random_state=12, shuffle=True)
-    scores = sklearn.model_selection.cross_val_score(
-        model, x, y, cv=cv, n_jobs=1)
-
-    print(len(scores), scores)
-    print(f"cross_validation: {np.mean(np.absolute(scores))}")
-
 def adaboosting():
-    boost = AdaBoostClassifier(sklearn.tree.DecisionTreeClassifier(max_depth=7),
-                               n_estimators=n, random_state=1)
+    boost = AdaBoostClassifier(sklearn.tree.DecisionTreeClassifier(max_depth=3),
+                               n_estimators=n, random_state=R)
     boost.fit(X_train, Y_train)
     return boost
 
+
+def grad_boosting():
+    gb_clf = GradientBoostingClassifier(n_estimators=n, learning_rate=0.44,
+                                        max_features=10, max_depth=3, random_state=R)
+    gb_clf.fit(X_train, Y_train)
+    return gb_clf
+
+
+def cross_validation(model, x, y, n):
+    cv = KFold(n_splits=n, random_state=R, shuffle=True)
+    scores = cross_val_score(model, x, y, cv=cv, n_jobs=1)
+
+    accuracy = -1 if isinstance(X_val, int) else round(model.score(X_val, Y_val), 3)
+    validation = round(1 - np.mean(np.absolute(scores)), 3)
+
+    print(len(scores), 1 - np.array(scores))
+    print(f"Accuracy score={accuracy} - cross_validation= {validation}")
+
+
 if __name__ == "__main__":
+    R = 1  # Random state
+    gaps = 104
+
+    def warn(*args, **kwargs):
+        pass
+    import warnings
+    warnings.warn = warn
+
+
     if not os.path.exists(const.TEST_DATASET) or not os.path.exists(const.TRAIN_DATASET):
         print("The file given does not exist, please check the path")
         raise FileNotFoundError
     else:
         data = pd.read_csv(const.TRAIN_DATASET)  # Import dataset
         X_train, Y_train = clean_x_and_y(data)
+        X_val = Y_val = 0
 
-    gaps = 104
+    # X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.30, random_state=R)
+
     n = int(np.ceil(len(X_train) / gaps))
-
-    # print("Starting Simple Classification Trees method")
-    # clf = classification_tree()
-    # cross_validation(clf, X_train, Y_train, n)
     #
-    # print("\nStarting Random Forrest method")
-    # rfc = random_forest()
-    # cross_validation(rfc, X_train, Y_train, n)
-    #
-    # print("\nStarting Bagging method")
-    # bag = bagging()
-    # cross_validation(bag, X_train, Y_train, n)
+    print("- - - - Starting Simple Classification Trees method - - - - ")
+    cross_validation(classification_tree(), X_train, Y_train, n)
 
-    print("\nStarting Boosting method")
-    boost = adaboosting()
-    cross_validation(boost, X_train, Y_train, n)
+    print("- - - - Starting Random Forrest method - - - - ")
+    cross_validation(random_forest(), X_train, Y_train, n)
+
+    print("- - - - Starting Bagging method - - - - ")
+    cross_validation(bagging(), X_train, Y_train, n)
+
+    print("- - - - Starting Boosting method - - - - ")
+    cross_validation(adaboosting(), X_train, Y_train, n)
+
+    print("- - - - Starting Gradient Boosting method - - - - ")
+    cross_validation(grad_boosting(), X_train, Y_train, n)
